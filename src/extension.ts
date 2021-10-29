c2392399513ccf4be9118de861140ac32aaba18d
洛竹 <youngjuning@163.com>
2021-10-29 11:06:26 +0800

    feat: support NPM_PACKAGE

--- src/extension.ts
+++ src/extension.ts
@@ -4,6 +4,7 @@ import has from 'lodash.has';
 const { init, localize } = require('vscode-nls-i18n');
 
 const NPM_TRENDS = 'https://www.npmtrends.com';
+const NPM_PACKAGE = 'https://www.npmjs.com/package';
 
 export function activate(context: vscode.ExtensionContext) {
   console.log('Congratulations, your extension "vscode-npm-trends" is now active!');
@@ -26,6 +27,8 @@ export function activate(context: vscode.ExtensionContext) {
     ) {
       const str = new vscode.MarkdownString();
       str.appendText(`${localize('npm-trends.key')}: ${NPM_TRENDS}/${packageName}`);
+      str.appendText(`\n\n`);
+      str.appendText(`${localize('npm-package.key')}: ${NPM_PACKAGE}/${packageName}`);
       return {
         contents: [str],
       };
