254c44b27e5872c6139a204d94e1cb5dab9b3559
洛竹 <youngjuning@163.com>
2021-10-30 15:23:55 +0800

    feat: 支持 jsdelivr

--- src/extension.ts
+++ src/extension.ts
@@ -5,6 +5,7 @@ const { init, localize } = require('vscode-nls-i18n');
 
 const NPM_TRENDS = 'https://www.npmtrends.com';
 const NPM_PACKAGE = 'https://www.npmjs.com/package';
+const JSDELIVR = 'https://www.jsdelivr.com/package/npm';
 
 export function activate(context: vscode.ExtensionContext) {
   console.log('Congratulations, your extension "vscode-npm-trends" is now active!');
@@ -29,6 +30,8 @@ export function activate(context: vscode.ExtensionContext) {
       str.appendText(`${localize('npm-trends.key')}: ${NPM_TRENDS}/${packageName}`);
       str.appendText(`\n\n`);
       str.appendText(`${localize('npm-package.key')}: ${NPM_PACKAGE}/${packageName}`);
+      str.appendText(`\n\n`);
+      str.appendText(`jedelivr: ${JSDELIVR}/${packageName}`);
       return {
         contents: [str],
       };
